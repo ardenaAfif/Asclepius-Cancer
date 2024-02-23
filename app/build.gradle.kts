bceb592f08d8aa1b26163bb1e3f14deae51d1109
aafif <aafifpratama@gmail.com>
2024-02-23 10:48:21 +0700

    Added ImageClassifierHelper for Classification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        mlModelBinding = true
     }
 }
 
@@ -44,6 +45,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -52,4 +55,7 @@ dependencies {
     implementation("com.github.yalantis:ucrop:2.2.8")
 
     // TODO: Tambahkan Library TensorFlow Lite
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 }
--- app/src/main/java/com/dicoding/cancer/helper/ImageClassifierHelper.kt
+++ app/src/main/java/com/dicoding/cancer/helper/ImageClassifierHelper.kt
@@ -1,16 +1,95 @@
 package com.dicoding.cancer.helper
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import com.dicoding.cancer.R
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.ops.CastOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
 
 
-class ImageClassifierHelper() {
+class ImageClassifierHelper(
+    private var threshold: Float = 0.1f,
+    private var maxResults: Int = 3,
+    private val modelName: String = "cancer_classification.tflite",
+    private val context: Context,
+    private val classifierListener: ClassifierListener?
+) {
+
+    private var imageClassifier: ImageClassifier? = null
+
+    init {
+        setupImageClassifier()
+    }
 
     private fun setupImageClassifier() {
-        // TODO: Menyiapkan Image Classifier untuk memproses gambar.
+        val optionsBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResults)
+
+        val baseOptionsBuilder = BaseOptions.builder().setNumThreads(4)
+
+        optionsBuilder.setBaseOptions(
+            baseOptionsBuilder.build()
+        )
+
+        try {
+            imageClassifier = ImageClassifier.createFromFileAndOptions(
+                context,
+                modelName,
+                optionsBuilder.build()
+            )
+        } catch (e: IllegalStateException) {
+            classifierListener?.onError(context.getString(R.string.onError))
+            Log.e(TAG, e.message.toString())
+        }
     }
 
     fun classifyStaticImage(imageUri: Uri) {
-        // TODO: mengklasifikasikan imageUri dari gambar statis.
+
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+            .add(CastOp(DataType.UINT8))
+            .build()
+
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(toBitmap(imageUri)))
+
+        val results = imageClassifier?.classify(tensorImage)
+        classifierListener?.onSuccess(results)
+    }
+
+    private fun toBitmap(imageUri: Uri): Bitmap {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source = ImageDecoder.createSource(context.contentResolver, imageUri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
+        }.copy(Bitmap.Config.ARGB_8888, true)
+    }
+
+    interface ClassifierListener {
+        fun onError(error: String)
+        fun onSuccess(
+            results: List<Classifications>?
+        )
+    }
+
+    companion object {
+        const val TAG = "ImageClassifierHelper"
     }
 
 }
